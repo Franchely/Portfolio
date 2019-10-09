dfd9fe2addb22987435780c9bbab039ee5833153
github username <franchely811@gmail.com>
2019-10-09 16:01:50 -0500

    adds content to projects page, adds styling

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,17 +1,60 @@
 import React, {Component} from "react"
+import Navbar from "./NavBar"
 
 class Projects extends Component {
 
     render() {
 
         return (
+            <div className="projects-page">
+                <Navbar></Navbar>
 
+                <h1 className="page-title">Projects</h1>
             <div className="projects-div">
-
-                <h1>Projects</h1>
-                <div>
-                    
+                <div className="project-div">
+                    <h2>Concert Ready</h2>
+                    <h3>Technologies used:</h3>
+                        React
+                        <br></br>
+                        Redux
+                        <br></br>
+                        Ruby on Rails API
+                        <br></br>
+                        Setlist.fm API
+                        <br></br>
+                        Custom CSS 
+                        <br></br>
+                    <h3>Video Demo</h3>
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/OTX6kVk3neg" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
+
+                <div className="project-div">
+                    <h2>Restaurant Journey</h2>
+                    <h3>Technologies used:</h3>
+                        React
+                        <br></br>
+                        Ruby on Rails API
+                        <br></br>
+                        Custom CSS 
+                        <br></br>
+                    <h3>Video Demo</h3>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/gJhzGkzZKLc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+
+                <div className="project-div">
+                    <h2>Critter.com</h2>
+                    <h3>Technologies used:</h3>
+                        JavaScript
+                        <br></br>
+                        Ruby on Rails API
+                        <br></br>
+                        Custom CSS 
+                        <br></br>
+                    <h3>Video Demo</h3>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/9_LR8jbqsKY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </div>
+
             </div>
         )
     }
