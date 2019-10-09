e2e71775514006752169f601c31b61488e73f6dd
github username <franchely811@gmail.com>
2019-10-09 11:38:37 -0500

    Adds links and styling to homepage

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react"
 import NavBar from "./NavBar"
+import {NavLink} from "react-router-dom"
 
 class Home extends Component {
 
@@ -10,6 +11,15 @@ class Home extends Component {
             <div className="home-div">
                 <div className="name">
                     Franchely <br></br>Portorreal
+                </div>
+                <div >
+                    <NavLink to="/about" className="home-links">About</NavLink>
+                </div>
+                <div>
+                    <NavLink to="/projects" className="home-links">Projects</NavLink>
+                </div>
+                <div>
+                    <NavLink to="/contact" className="home-links">Contact</NavLink>
                 </div>
             </div>
         )
